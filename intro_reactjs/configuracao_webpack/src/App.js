55934e2f08736ba4c6079f3af68d89839e3edf42
Andrea <andreamizuno@gmail.com>
2022-05-23 17:40:48 -0300

    add configuracao_webpack/

---
+++ intro_reactjs/configuracao_webpack/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Button from "./Button"
+
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem-vindo à nossa aula =D</p>
+      <div>Webpack 4</div>
+      <Button />
+    </div>
+  )
+}
+
+export default App
